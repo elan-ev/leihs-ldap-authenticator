ce587ade37a5b0f97c1b14e1ed2ba44e7b13e314
Lars Kiesow <lkiesow@uos.de>
2022-09-29 16:27:50 +0200

    Error Pages
    This patch adds error pages for several different error types instead of
    always just ending up with an internal server error and only logging the
    real error. Pages which now have custom error pages are:
    
    - token expired
    - invalid credentials
    - no token
    - invalid credentials
    - general error
    
    This fixes #5

--- leihsldap/authenticator.py
+++ leihsldap/authenticator.py
@@ -11,33 +11,21 @@ def response_url(sign_in_token_data, success_token):
 
 
 def token_data(token):
-    options = {}
-
-    # Fall back to demo token for development if one is provided
-    if not token:
-        print('Warning: Using demo token')
-        token = config('token', 'demo')
-
-    if config('token', 'allow_expired'):
-        options = {'verify_exp': False}
+    options = {'verify_exp': not config('token', 'allow_expired')}
     private_key = config('token', 'private_key')
     return jwt.decode(token, private_key, ['ES256'], options)
 
 
 def authenticate(token):
-    options = {}
-    if config('token', 'allow_expired'):
-        options = {'verify_exp': False}
-    private_key = config('token', 'private_key')
-    data = jwt.decode(token, private_key, ['ES256'], options)
-    email = data['email']
+    data = token_data(token)
 
     # generate success token
     iat = int(time.time())
     exp = iat + config('token', 'validity')
+    private_key = config('token', 'private_key')
     success_token = jwt.encode({
             'sign_in_request_token': token,
-            'email': email,
+            'email': data.get('email'),
             'iat': iat,
             'exp': exp,
             'success': True
