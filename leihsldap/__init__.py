722bd1ef7b99e928f35dbe4821bebf0f9c9c92b5
Lars Kiesow <lkiesow@uos.de>
2022-10-01 14:09:51 +0200

    Properly Apply GPL
    This patch properly applies the GPL to this project, adding license
    headers to all source files and adding a GitHub Actions workflow to
    check for files with missing headers.
    
    The check is far from being perfect, but should at least never cause
    false positives and should make reasonably sure that there is a header
    somewhere.

--- leihsldap/__init__.py
+++ leihsldap/__init__.py
@@ -13,28 +13,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-import argparse
-
-from leihsldap.config import update_configuration
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='LDAP based authentication handler for Leihs',
-        formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-    parser.add_argument(
-        '-c', '--config',
-        type=str,
-        default=None,
-        help='Path to a configuration file'
-    )
-    args = parser.parse_args()
-    if args.config:
-        update_configuration(args.config)
-
-    # Since `app` will use the configuration,
-    # load it only after we updated the configuration location
-    from leihsldap.web import app
-    app.run()
