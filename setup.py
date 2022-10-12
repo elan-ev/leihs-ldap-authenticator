46fd1c83354e73ee890cb288ac30ead317b08753
Lars Kiesow <lkiesow@uos.de>
2022-10-12 12:18:55 +0200

    Release 0.3
    - Internationalization Support
    - Documentation Improvements
    - Check Config via yamllint

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='leihs-ldap-authenticator',
     author='Lars Kiesow <kiesow@elan-ev.de>',
     license='GPLv3+',
-    version='0.2',
+    version='0.3',
     url='https://github.com/elan-ev/leihs-ldap-authenticator',
     packages=find_packages(),
     install_requires=read('requirements.txt').split(),
